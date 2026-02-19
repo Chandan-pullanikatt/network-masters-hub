9ab53503fbfe19a585c8b631af82c009e43a800f
Chandan Pullanikatt <chandanpullanikatt@gmail.com>
2026-02-19 15:12:46 +0530

    strapifix3

--- backend/src/api/order/controllers/manual.ts
+++
@@ -1,8 +0,0 @@
-import { Core } from '@strapi/strapi';
-
-export default ({ strapi }: { strapi: Core.Strapi }) => ({
-    async process(ctx) {
-        console.log("Manual controller 'process' action hit");
-        ctx.send({ message: "It works with manual.ts!" });
-    }
-});
--- backend/src/api/order/routes/01-custom-order.ts
+++ backend/src/api/order/routes/01-custom-order.ts
@@ -1,11 +1,9 @@
-console.log("[DEBUG] Loading custom manual-payment route...");
-
 export default {
     routes: [
         {
             method: "POST",
-            path: "/manual-payment",
-            handler: "api::order.manual.process",
+            path: "/orders/manual-payment",
+            handler: "api::order.order.createManualOrder",
             config: {
                 auth: false,
             },
